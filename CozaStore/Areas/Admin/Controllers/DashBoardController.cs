e8f291ce97e7656f68333da3e99678c82be7553e
ankhoa.dev@gmail.com <ankhoa.dev@gmail.com>
2024-08-02 20:43:13 +0700

    Add project files.

---
+++ CozaStore/Areas/Admin/Controllers/DashBoardController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CozaStore.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class DashBoardController : Controller
+    {
+        [Route("/admin")]
+        [Route("/admin/dashboard")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ CozaStore/Areas/Admin/Controllers/SizeController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CozaStore.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class SizeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
